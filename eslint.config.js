5b032cc13fb224aaa8c5afa83867d6c65a7996b2
bx <boweixu929@gmail.com>
2026-02-08 09:20:50 -0500

    Add ESLint and Husky for code linting and pre-commit hooks
    - Added a lint script to package.json for running ESLint on TypeScript files.
    - Included ESLint and Husky as development dependencies.

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import tseslint, { plugin } from 'typescript-eslint';
+
+export default [
+    {
+        ignores: ['dist', 'node_modules', 'build'],
+    },
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                project: './tsconfig.json',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': plugin,
+        },
+        rules: {
+            ...tseslint.configs.recommended[0].rules,
+            '@typescript-eslint/no-unused-vars': 'warn',
+            '@typescript-eslint/explicit-function-return-types': 'warn',
+        },
+    },
+];
